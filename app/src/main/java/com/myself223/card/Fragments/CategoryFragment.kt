ab5829e36053dc914af08569fe2404112af60a10
Kudbuhon08 <qweimxs@gmail.com>
2024-01-22 20:22:43 +0300

    V2

---
+++ app/src/main/java/com/myself223/card/Fragments/CategoryFragment.kt
@@ -0,0 +1,28 @@
+package com.myself223.card.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.myself223.card.R
+import com.myself223.card.databinding.FragmentCategoryBinding
+import com.myself223.card.databinding.FragmentHomeBinding
+
+class CategoryFragment : Fragment() {
+    private var navController: NavController? = null
+    lateinit var binding: FragmentCategoryBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentCategoryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
+
+
---
+++ app/src/main/java/com/myself223/card/Fragments/HomeFragment.kt
@@ -0,0 +1,34 @@
+package com.myself223.card.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.myself223.card.R
+import com.myself223.card.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+    private var navController: NavController? = null
+
+
+
+
+
+    override fun onCreateView(
+
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+
+
+
+}
