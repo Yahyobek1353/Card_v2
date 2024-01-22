4620e0f0b61ffc18ebd8a8dced824f07b01d9960
Kudbuhon08 <qweimxs@gmail.com>
2024-01-22 20:59:24 +0300

    v2.1

--- app/src/main/java/com/myself223/card/Fragments/HomeFragment.kt
+++ app/src/main/java/com/myself223/card/Fragments/HomeFragment.kt
@@ -5,26 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
+import com.myself223.card.App
 import com.myself223.card.R
 import com.myself223.card.databinding.FragmentHomeBinding
 
+@Suppress("UNREACHABLE_CODE")
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
     private var navController: NavController? = null
-
-
-
-
-
     override fun onCreateView(
-
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+        if (App.prefs.isShow()){
+
+        }else{
+            findNavController().navigate(R.id.onBoard)
+            App.prefs.changeShow(true)
+        }
 
     }
 
